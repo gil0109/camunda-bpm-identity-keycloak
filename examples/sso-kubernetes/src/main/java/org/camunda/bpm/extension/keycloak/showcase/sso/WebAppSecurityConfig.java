e46c8ce07525da0ce5cfed60b67a7eb6524f8d16
Karim Gillani <karim.gillani@gov.bc.ca>
2019-11-13 09:36:11 -0800

    Test Fix

--- examples/sso-kubernetes/src/main/java/org/camunda/bpm/extension/keycloak/showcase/sso/WebAppSecurityConfig.java
+++ examples/sso-kubernetes/src/main/java/org/camunda/bpm/extension/keycloak/showcase/sso/WebAppSecurityConfig.java
@@ -14,57 +14,57 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.web.context.request.RequestContextListener;
 
 /**
- * Camunda Web application SSO configuration for usage with Auth0IdentityProviderPlugin.
+ * Camunda Web application SSO configuration for usage with
+ * Auth0IdentityProviderPlugin.
  */
 @ConditionalOnMissingClass("org.springframework.test.context.junit4.SpringJUnit4ClassRunner")
 @Configuration
 @EnableOAuth2Sso
 public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	
-    	http
-    	.csrf().ignoringAntMatchers("/api/**")
-    	.and()
-        .antMatcher("/**")
-        .authorizeRequests()
-          .antMatchers("/app/**")
-          .authenticated()
-        .anyRequest()
-          .permitAll()
-        ;
 
-    }
+	/**
+	 * @Override protected void configure(HttpSecurity http) throws Exception {
+	 * 
+	 *           http .csrf().ignoringAntMatchers("/api/**", "/engine-rest/**")
+	 *           .and() .antMatcher("/**") .authorizeRequests()
+	 *           .antMatchers("/app/**") .authenticated() .anyRequest() .permitAll()
+	 *           ;
+	 * 
+	 *           }
+	 */
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Bean
-    public FilterRegistrationBean containerBasedAuthenticationFilter(){
+	public FilterRegistrationBean containerBasedAuthenticationFilter() {
 
-        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
-        filterRegistration.setFilter(new ContainerBasedAuthenticationFilter());
-        filterRegistration.setInitParameters(Collections.singletonMap("authentication-provider", "org.camunda.bpm.extension.keycloak.showcase.sso.KeycloakAuthenticationProvider"));
-        filterRegistration.setOrder(101); // make sure the filter is registered after the Spring Security Filter Chain
-        filterRegistration.addUrlPatterns("/app/*");
-        return filterRegistration;
-    }
- 
+		FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+		filterRegistration.setFilter(new ContainerBasedAuthenticationFilter());
+		filterRegistration.setInitParameters(Collections.singletonMap("authentication-provider",
+				"org.camunda.bpm.extension.keycloak.showcase.sso.KeycloakAuthenticationProvider"));
+		filterRegistration.setOrder(101); // make sure the filter is registered after the Spring Security Filter Chain
+		filterRegistration.addUrlPatterns("/app/*");
+		return filterRegistration;
+	}
+
 	@Bean
 	@Order(0)
 	public RequestContextListener requestContextListener() {
-	    return new RequestContextListener();
+		return new RequestContextListener();
 	}
-	
+
 	/**
 	 * Configures the OAuth2 TokenStore for Redis Cache usage.
+	 * 
 	 * @param redisConnectionFactory the Redis Connection Factoryf
 	 * @return Redis prepared TokenStore
 	 */
-	/* Redis Session Cache not yet in use
-	@Bean
-	@Primary
-	public TokenStore tokenStore(RedisConnectionFactory redisConnectionFactory) {
-	    return new RedisTokenStore(redisConnectionFactory);
-	}
-	*/
+	/*
+	 * Redis Session Cache not yet in use
+	 * 
+	 * @Bean
+	 * 
+	 * @Primary public TokenStore tokenStore(RedisConnectionFactory
+	 * redisConnectionFactory) { return new RedisTokenStore(redisConnectionFactory);
+	 * }
+	 */
 }
